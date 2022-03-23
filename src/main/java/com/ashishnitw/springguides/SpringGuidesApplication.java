e405db0680a4757b2af85bcc640d28ec1c049802
Ashish Ranjan <ashish.ranjan@payu.in>
2022-03-23 19:16:00 +0530

    created new project

---
+++ src/main/java/com/ashishnitw/springguides/SpringGuidesApplication.java
@@ -0,0 +1,13 @@
+package com.ashishnitw.springguides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGuidesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGuidesApplication.class, args);
+	}
+
+}
